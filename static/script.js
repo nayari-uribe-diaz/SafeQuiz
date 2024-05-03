2a07a1fb548bcb785540d80682630083bad929d1
Nayari Uribe Diaz <nayariu7@gmail.com>
2024-05-03 13:53:25 -0700

    update

---
+++ static/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$("#q1").click(function(){
+		$("#q1answer").toggle();
+	});
+	/*$("#p1").click(function() {
+		$(".disappear").toggle();
+	});
+	$("#p2").dblclick(function() {
+		$("#p2").css({"color":"red"});
+	});
+	$("h1").click(function() {
+		$("p").css({"color":"red", "background-color": "yellow"});
+		$("img").css("border","25px solid black");
+	}); */
+});
